804ee12ab235cbc6752edf89f44e138e74dae2ce
Tornike Janashia <t.janashia11@gmail.com>
2020-06-04 22:55:14 +0400

    feat(part2): add 2.14*: Data for countries, step3 request

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,7 +1,27 @@
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Country = (props) => {
+    const api_key = process.env.REACT_APP_API_KEY
+    const [weather, SetWeather] = useState();
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+          .get(`https://api.weatherstack.com/current?access_key=${api_key}&query=${props.capital}`)
+          .then(response => {
+            console.log(response.data)
+            /*idea is clear, 
+            but I'm getting 
+            {code: 105, type: "https_access_restricted", 
+            info: "Access Restricted - Your current Subscription Plan does not support HTTPS Encryption."}
+            skipping this exercise
+            */
+          })
+      }, [])
+
     return(
         <div>
                 <h1>{props.name}</h1>
