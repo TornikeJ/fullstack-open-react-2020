97f2c3b06854aad63e28355c477d91a790e47c0a
Tornike Janashia <t.janashia11@gmail.com>
2020-06-09 23:23:33 +0400

    feat(part4): add 4.10: Blog list tests, step3 exercise

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,30 +1,43 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../controller/blog')
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../controller/blog");
 
-const api = supertest(app)
+const api = supertest(app);
 
-test('blogs are returned as json', async () => {
+test("blogs are returned as json", async () => {
   await api
-    .get('/api/blogs')
+    .get("/api/blogs")
     .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+    .expect("Content-Type", /application\/json/);
+});
 
-test('returned blogs length', async () => {
-  const response = await api.get('/api/blogs')
+test("returned blogs length", async () => {
+  const response = await api.get("/api/blogs");
 
-  expect(response.body).toHaveLength(1);
+  expect(response.body).toHaveLength(3);
+});
 
-})
-
-test('check id property existence', async () => {
-  const response = await api.get('/api/blogs')
+test("check id property existence", async () => {
+  const response = await api.get("/api/blogs");
 
   expect(response.body[0].id).toBeDefined();
+});
 
-})
+test("check post method", async () => {
+  const beforePost = await api.get("/api/blogs");
+  
+  await api.post("/api/blogs").send({
+    title: "x",
+    author: "anon",
+    url: "lala",
+    likes: 1,
+  });
+
+  const afterpost=await api.get("/api/blogs");
+  
+  expect(beforePost.body).toHaveLength(afterpost.body.length-1);
+});
 
 afterAll(() => {
-  mongoose.connection.close()
-})
+  mongoose.connection.close();
+});
