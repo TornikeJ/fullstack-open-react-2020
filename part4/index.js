c3e896d1cd2b37c4a58a3f81c4258b5a5d7141c3
Tornike Janashia <t.janashia11@gmail.com>
2020-06-10 22:17:44 +0400

    feat(part4): add 4.18: bloglist expansion, step6 exercise

--- part4/index.js
+++ part4/index.js
@@ -3,11 +3,13 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controller/blog');
 const userRouter = require('./controller/users');
+const loginRouter = require('./controller/login')
 const config = require('./utils/config')
 
 
 app.use(cors())
 app.use(express.json())
+app.use(loginRouter);
 app.use(blogsRouter);
 app.use(userRouter);
 
