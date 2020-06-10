1ad26095c135cbf694713e56e0c7d5d31f9107c8
Tornike Janashia <t.janashia11@gmail.com>
2020-06-10 21:04:51 +0400

    feat(part4): add 4.16*: bloglist expansion, step4 exercise

--- part4/controller/users.js
+++ part4/controller/users.js
@@ -6,13 +6,19 @@ const User = require('../models/user')
 usersRouter.get('/api/users', async(reques,response) =>{
     const users= await User.find({});
     console.log(users);
-    response.json(users);
+    response.send(200).json(users);
 })
 
 usersRouter.post('/api/users', async (request, response) => {
   const body = request.body
 
-  console.log(body);
+  console.log(body.password);
+
+  if(body.password.length < 3){
+      return response.status(401).json({
+          error:'Password length must be at least 3 characters'
+        });
+  }
 
   const saltRounds = 10
   const password = await bcrypt.hash(body.password, saltRounds)
@@ -23,9 +29,13 @@ usersRouter.post('/api/users', async (request, response) => {
     password,
   })
 
-  const savedUser = await user.save()
-
-  response.json(savedUser)
+  try{
+      const savedUser = await user.save()
+      response.json(savedUser)
+  } catch(exception){
+      console.log(exception.message);
+      return response.status(401).json({error:exception.message})
+  }
 })
 
 module.exports = usersRouter
--- part4/models/user.js
+++ part4/models/user.js
@@ -1,30 +1,40 @@
-const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema({
-  username: {type:String, unique:true},
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  
   name: String,
-  password: String,
+  password: {
+    type: String,
+    required: true
+  },
+
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Blog'
-    }
+      ref: "Blog",
+    },
   ],
-})
+});
 
-userSchema.set('toJSON', {
+userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
     // the passwordHash should not be revealed
-    delete returnedObject.password
-  }
-})
+    delete returnedObject.password;
+  },
+});
 
 userSchema.plugin(uniqueValidator);
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
