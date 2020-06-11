a56c175c669c80235606a5a42c70b0c1140c1108
Tornike Janashia <t.janashia11@gmail.com>
2020-06-11 22:28:45 +0400

    feat(part5): add 5.7* Blog list frontend, step7 exercise

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, {useState} from 'react'
+ 
+const Blog = ({ blog }) => {
+  const [showList, setShowList]=useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return(
+  <div style={blogStyle}>
+    <div>
+      {blog.title} {blog.author}
+    <button onClick={()=>setShowList(!showList)}>{showList? 'hide':'show'}</button>
+    </div>
+    {
+      showList ?
+    <div>
+      <p>
+        {blog.url}  
+      </p>      
+      <p>
+        {blog.likes} <button >Like</button>
+      </p>      
+    </div>:null
+    }
   </div>
-)
+  )}
 
 export default Blog
