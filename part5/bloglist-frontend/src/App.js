8981bc7d7e5473151d17ea16efac390b93335c87
Tornike Janashia <t.janashia11@gmail.com>
2020-06-11 23:43:13 +0400

    feat(part5): add 5.9*: Blog list frontend, step9 exercise

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -129,7 +129,7 @@ const App = () => {
           <Togglable buttonLabel='new note' ref={blogFormRef}>
             {blogForm()}
           </Togglable>
-          {blogs.map((blog) => (
+          {blogs.sort((a,b)=> b.likes - a.likes).map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
         </div>
