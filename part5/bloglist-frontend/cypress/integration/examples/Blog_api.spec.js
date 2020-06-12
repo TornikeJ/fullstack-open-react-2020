6ea31c8460da9223f6a2d4c6064d695de53321c9
Tornike Janashia <t.janashia11@gmail.com>
2020-06-12 17:22:15 +0400

    feat(part5): add 5.17: bloglist end to end testing, step1 exercise

---
+++ part5/bloglist-frontend/cypress/integration/examples/Blog_api.spec.js
@@ -0,0 +1,9 @@
+describe('Blog app', function() {
+    beforeEach(function() {
+      cy.visit('http://localhost:3000')
+    })
+  
+    it('Login form is shown', function() {
+      cy.contains('Login')
+    })
+  })
