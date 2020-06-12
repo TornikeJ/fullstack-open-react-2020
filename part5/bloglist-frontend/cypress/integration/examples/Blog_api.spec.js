9ec0e1f2b6b66d4da073f24b87f21dbcd0b1a6b2
Tornike Janashia <t.janashia11@gmail.com>
2020-06-12 17:32:14 +0400

    feat(part5): add 5.18: bloglist end to end testing, step2 exercise

--- part5/bloglist-frontend/cypress/integration/examples/Blog_api.spec.js
+++ part5/bloglist-frontend/cypress/integration/examples/Blog_api.spec.js
@@ -6,4 +6,20 @@ describe('Blog app', function() {
     it('Login form is shown', function() {
       cy.contains('Login')
     })
+
+    describe('Login',function() {
+      it('succeeds with correct credentials', function() {
+        cy.get('#username').type('test5')
+        cy.get('#password').type('test')
+        cy.get('#submitLogin').click()
+        cy.contains('test logged in')
+      })
+  
+      it('fails with wrong credentials', function() {
+        cy.get('#username').type('test5')
+        cy.get('#password').type('test5')
+        cy.get('#submitLogin').click()
+        cy.contains('Wrong credentials')
+      })
+    })
   })
